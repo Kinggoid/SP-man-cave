f4e095650164c6eeb62c3051441509f4969a93ce
Kinggoid <thijmespambox@gmail.com>
2020-02-04 15:02:22 +0100

    Opdracht 3

--- Lijstcheck.py
+++ Lijstcheck.py
@@ -1,2 +1,27 @@
-list = [1,1,1,1,1,2,2,2,2,2,2,3,4,5,6,5,6,4]
-def count(list):
+list2 = [1, 8, 5, 1, 1, 2, 7, 2, 3, 9, 10, 3, 6, 5, 1, 7, 6, 4]
+
+
+def counte(list1):
+    x = int(input('Getal: '))
+    y = 0
+
+    for i in list1:
+        if i == x:
+            y += 1
+    return y
+
+
+def opeenvolgendverschil(list1):
+    list3 = []
+    ll = 0
+
+    for i in list1:
+        ll += 1
+
+    for i in range(0, ll - 1):
+        list3 += abs(list1[i] - list1[i+1])
+    return max(list3)
+
+
+print(counte(list2))
+print(opeenvolgendverschil(list2))
