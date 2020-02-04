30ae082cde993af414e24d4e385fe4aaf6f132e6
Kinggoid <thijmespambox@gmail.com>
2020-02-04 14:39:37 +0100

    Opdracht 2

--- Pyramid.py
+++ Pyramid.py
@@ -12,3 +12,17 @@ while x != groot + 1:
 while x != 0:
     print(x * '*')
     x -= 1
+
+
+for i in range(1, groot + 1):
+    print((groot - i) * ' ' + i * '*')
+for i in range(1, groot + 1):
+    print(i * ' ' + (groot - i) * '*')
+
+x = 1
+while x != groot + 1:
+    print((groot - x + 1) * ' ' + x * '*')
+    x += 1
+while x != 0:
+    print((groot - x + 1) * ' ' + x * '*')
+    x -= 1
