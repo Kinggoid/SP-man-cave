a8b686dcb5cd88396aab94d5f1793637c05f66e5
Kinggoid <thijmespambox@gmail.com>
2020-02-06 17:03:33 +0100

    Opdracht 9

--- Cyclisch verschuiven.py
+++ Cyclisch verschuiven.py
@@ -2,7 +2,7 @@ ch = '10001011'
 
 
 def verschuiven(bits, n):
-    eerste, tweede = bits[0:n], bits[n:]
+    eerste, tweede = bits[0:n], bits[n:] #Hiermee split ik de bits in twee delen
     return tweede + eerste
 
 print(verschuiven(ch, 4))
