7891dcdc9c46629a9a4729531138a2eedda34fd5
Kinggoid <thijmespambox@gmail.com>
2020-02-07 13:01:05 +0100

    Opdracht 10

---
+++ SP/Eerste jaar/Fibonaci.py
@@ -0,0 +1,11 @@
+def fibonaci(een, twee, n):
+    antwoord = een + twee
+    if n == 0 or n == 1:
+        return 0
+    elif n == 2:
+        return antwoord
+    else:
+        return fibonaci(twee, antwoord, n-1)
+
+
+print(fibonaci(0, 1, int(input('Input getal: '))))
